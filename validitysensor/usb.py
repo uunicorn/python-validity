19871889bd2b6ba7972b3104c5e220b714ad5895
Milad Olad <milad.olad@gmail.com>
2026-05-27 18:21:18 -0400

    Add USB device entry and firmware tables for 138a:0092
    Add DEV_92 (0x138a, 0x0092) to SupportedDevices in usb.py.
    Map device 0x0092 to use blobs_9d in blobs.py.
    Add proper firmware partition table entries for DEV_92 in firmware_tables.py.
    
    This sensor is found in HP EliteBook x360 1030 G2 and similar HP laptops.
    It uses sensor type 0x1825 with ROM product=0x30 major=0x6 minor=0x7.

--- validitysensor/usb.py
+++ validitysensor/usb.py
@@ -18,6 +18,7 @@ class SupportedDevices(Enum):
     DEV_97 = (0x138a, 0x0097)
     DEV_9d = (0x138a, 0x009d)
     DEV_9a = (0x06cb, 0x009a)
+    DEV_92 = (0x138a, 0x0092)
 
     @classmethod
     def from_usbid(cls, vendorid, productid):
