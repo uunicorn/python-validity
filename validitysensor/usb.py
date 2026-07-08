a7c1de81b00ecf78accee4d9ee16d7d0ec40b663
Killersparrow1 <139992627+Killersparrow1@users.noreply.github.com>
2026-07-08 17:58:33 +0530

    Add support for 138a:00ab and 06cb:00b7 devices
    These Validity/Synaptics sensors (sensor type 0xd51) are found in:
    - HP EliteBook 840 G5 (138a:00ab)
    - HP G6 series (06cb:00b7)
    
    The device IDs are already present in the Fedora Copr
    package (python3-validity-0.15) and confirmed working.

--- validitysensor/usb.py
+++ validitysensor/usb.py
@@ -18,6 +18,8 @@ class SupportedDevices(Enum):
     DEV_97 = (0x138a, 0x0097)
     DEV_9d = (0x138a, 0x009d)
     DEV_9a = (0x06cb, 0x009a)
+    DEV_AB = (0x138a, 0x00ab)
+    DEV_B7 = (0x06cb, 0x00b7)
 
     @classmethod
     def from_usbid(cls, vendorid, productid):
